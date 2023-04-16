7c1c14bdedaf1d9ef3c578f8ae9d4f897ca4b503
dacasfilipe <filiperdacas@gmail.com>
2023-04-16 20:15:05 -0300

    adicionado react hook form para inclusão de  livros

--- reactjs/web-app/src/components/InclusaoLivros.js
+++ reactjs/web-app/src/components/InclusaoLivros.js
@@ -1,31 +1,42 @@
+import {useForm} from 'react-hook-form';
+
 const InclusaoLivros = () => {
+    //register serve para definir os nomes dos campos do formulário e validações
+    //handleSubmit serve para indicar o método a ser chamado quando o formulario for enviado pelo onSubmit
+    const {register, handleSubmit} = useForm();
+
+    //método chamado ao enviar o formulario(onSubmit)
+    const salvar = (campos) => {
+        //JSON.stringify() serve para transformar um objeto Javascript em uma string JSON
+        alert(JSON.stringify(campos));
+    }
     return (
         <div className="container">
             <h4 className="fst-italic mt-3">Inclusão </h4>
-            <form>
+            <form onSubmit={handleSubmit(salvar)}>
                 <div className="form-group">
                     <label htmlFor="titulo">Título:</label>
-                    <input type="text" className="form-control" id="titulo" required autofocus/>
+                    <input type="text" className="form-control" id="titulo" required autofocus {...register("titulo")}/>
                 </div>
                 <div className="form-group mt-2">
                     <label htmlFor="autor">Autor:</label>
-                    <input type="text" className="form-control" id="autor" required/>
+                    <input type="text" className="form-control" id="autor" required {...register("autor")}/>
                 </div>
                 <div className="form-group mt-2">
                     <label htmlFor="foto">URL da foto:</label>
-                    <input type="url" className="form-control" id="foto" required/>
+                    <input type="url" className="form-control" id="foto" required {...register("foto")}/>
                 </div>
                 <div className="row mt-2">
                     <div className="col-sm-4">
                         <div className="form-group">
                             <label htmlFor="ano">Ano de Publicação:</label>
-                            <input type="number" className="form-control" id="ano" required/>
+                            <input type="number" className="form-control" id="ano" required {...register("ano")}/>
                         </div>
                     </div>
                     <div className="col-sm-8">
                         <div className="form-group">
                             <label htmlFor="preco">Preço R$:</label>
-                            <input type="number" className="form-control" id="preco" step="0.01" required/>
+                            <input type="number" className="form-control" id="preco" step="0.01" required {...register("preco")}/>
                         </div>
                     </div>
                 </div>
