bfb3504c196aba9103d4871e294822c8b980ce93
dacasfilipe <filiperdacas@gmail.com>
2023-04-17 07:52:43 -0300

    implementado resumos de livros com gráficos

--- reactjs/web-app/src/components/ResumoLivros.js
+++ reactjs/web-app/src/components/ResumoLivros.js
@@ -1,5 +1,80 @@
-const ResumoLivros = () => {
-    return <h2>Resumo</h2>
-};
+import {useState, useEffect} from 'react';
+import { Chart } from 'react-google-charts';
+import {inAxios} from "../config_axios";
 
+
+const ResumoLivros = () => {
+    //return <h2>Resumo</h2>
+    const [resumo, setResumo] = useState([]);
+    const [grafico, setGrafico] = useState([]);
+    const obterDados = async () => {
+        try{
+            const dadosResumo = await inAxios.get("livros/dados/resumo");
+            setResumo(dadosResumo.data);
+
+            const dadosGrafico = await inAxios.get("livros/dados/grafico");
+            //cria uma array e adiciona a primeira linha do grafico
+            const arrayGrafico = [["Ano", "R$ Total"]];
+            //percorre cada linha do JSON e adiciona no array
+            dadosGrafico.data.map((dado) => {
+                arrayGrafico.push([dado.ano.toString(), dado.total]);
+            });
+            setGrafico(arrayGrafico);
+        }catch(error){
+            alert(`Erro: ..Não foi possível obter os dados: ${error}`);
+        }
+    }
+
+
+//define o método que será executado assim que o componente for renderizado
+useEffect(() => {
+    obterDados();
+},[]);
+
+return(
+    <div className="container">
+        <h4 className="mt-3">Resumo</h4>
+        <span className="btn btn-outline-primary btn-lg">
+            <p className="badge bg-danger">{resumo.num}</p>
+            <p>N de livros cadastrados</p>
+        </span>
+        <span className="btn btn-outline-primary btn-lg mx-2">
+            <p className="badge bg-danger">
+                {Number(resumo.soma).toLocaleString("pt-br",{minimumFractionDigits : 2})}
+            </p>
+            <p>Total investido em livros</p>
+        </span>
+        <span className="btn btn-outline-primary btn-lg me-2">
+            <p className="badge bg-danger">
+                {Number(resumo.maior).toLocaleString("pt-br",{minimumFractionDigits : 2})}
+            </p>
+            <p>Maior preço cadastrado</p>
+        </span>
+        <span className="btn btn-outline-primary btn-lg">
+            <p className="badge bg-danger">
+                {Number(resumo.media).toLocaleString("pt-br",{minimumFractionDigits : 2})}
+            </p>
+            <p>Preço médio dos livros</p>
+        </span>
+
+        <div className='d-flex justify-content-center'>
+            <Chart 
+                width={1000}
+                heigth={420}
+                chartType="ColumnChart"
+                loader={<div>Carregando Gráfico...</div>}
+                data={grafico}
+                options={{
+                    title: 'Total de Investimento em Livros - por Ano de Publicação',
+                    chartArea: {width: '80%'},
+                    hAxis: {title: 'Ano de Publicação'},
+                    vAxis: {title: 'Preço Acumulado R$'}, 
+                    legend: {position: 'none'},              
+                    }
+                }
+            />
+        </div>
+    </div>
+);
+};//fim da função ResumoLivros
 export default ResumoLivros;
