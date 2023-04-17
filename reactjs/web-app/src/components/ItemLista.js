af757d87b324f42a2afe954a9a51e2f532d784f9
dacasfilipe <filiperdacas@gmail.com>
2023-04-16 22:30:41 -0300

    incluido os componentes itemlista, manutencaolivros

---
+++ reactjs/web-app/src/components/ItemLista.js
@@ -0,0 +1,24 @@
+import "./ItemLista.css";
+
+const ItemLista = (props) => {
+    return (
+        <tr>
+            <td>{props.id}</td>
+            <td>{props.titulo}</td>
+            <td>{props.autor}</td>
+            <td>{props.ano}</td>
+            <td class="text-end">
+                {Number(props.preco).toLocaleString("pt-br", {minimumFractionDigits:2})}
+            </td>
+            <td class="text-center">
+                <img src={props.foto} alt="capa do livro" width="75px"/>
+            </td>
+            <td class="text-center">
+                <i className="exclui text-danger fw-bold" title="Excluir">&#10008;</i>
+                <i className="altera text-sucess fw-bold ms-2" title="Alterar">&#36;</i>
+            </td>
+        </tr>
+    );
+};
+
+export default ItemLista;
--- reactjs/web-app/src/components/ManutencaoLivros.js
+++ reactjs/web-app/src/components/ManutencaoLivros.js
@@ -1,5 +1,72 @@
+import { useState, useEffect } from "react";
+import { inAxios } from "../config_axios";
+import {ItemLista} from "./ItemLista";
+
 const ManutencaoLivros = () => {
-    return <h2>Manutenção</h2>
-}
+    const [livros, setLivros] = useState([]);
+
+    const obterLista = async () => {
+        try{
+            const lista = await inAxios.get("livros");
+            setLivros(lista.data);
+        }catch(error){
+            alert(`Erro: ..Não foi possível obter os dados: ${error}`);
+        }
+    }
+
+
+//define o método que será executado assim que o componente for renderizado
+useEffect(() => {
+    obterLista();
+},[]);
+
+
+    return (
+       <div className="container">
+        <div className="row">
+            <div className="col-sm-7">
+                <h4 className="fst-italic mt-3">Manutenção</h4>
+            </div>
+            <div className="col-sm-5">
+                <form>
+                    <div className="input-group mt-3">
+                        <input type="text" className="form-control" placeholder="Titulo ou Autor" required />
+                        <input type="submit" className="btn btn-primary" value="Pesquisar" />
+                        <input type="button" className="btn btn-danger" value="Todos" />
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <table className="table table-striped mt-3">
+            <thead>
+                <tr>
+                    <th>Cód.</th>
+                    <th>Titulo</th>
+                    <th>Autor</th>
+                    <th>Ano</th>
+                    <th>Preço</th>
+                    <th>Foto</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {livros.map((livro) => (
+                    <ItemLista
+                        key={livro.id}
+                        id={livro.id}
+                        titulo={livro.titulo}
+                        autor={livro.autor}
+                        ano={livro.ano}
+                        preco={livro.preco}
+                        foto={livro.foto}
+                    />
+                ))}
+            </tbody>
+        </table>
+
+       </div> 
+    );
+};
 
 export default ManutencaoLivros;
